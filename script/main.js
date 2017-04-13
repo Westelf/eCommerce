08cbb1a87a2897bf793dc25c0755bd1e4abc3600
Lyubomir Bandrov <lyubomir.bandrov@strypes.eu>
2017-04-13 16:37:09 +0300

    Router changes

--- script/main.js
+++ script/main.js
@@ -1,7 +1,8 @@
 var content;
 
 var configs = {
-  lang : "bg",
+  lang: "bg",
+  url: "http://localhost:6060/"
 }
 
 
@@ -11,18 +12,38 @@ window.onload = function () {
 
 
 
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyCMKwdSlr2UTyg_Yip5CUvQvIId94qfuOY",
-  //   authDomain: "teamkhameleon.firebaseapp.com",
-  //   databaseURL: "https://teamkhameleon.firebaseio.com",
-  //   storageBucket: "teamkhameleon.appspot.com",
-  //   messagingSenderId: "818068003629"
-  // };
-  // firebase.initializeApp(config);
+// Initialize Firebase
+// var config = {
+//   apiKey: "AIzaSyCMKwdSlr2UTyg_Yip5CUvQvIId94qfuOY",
+//   authDomain: "teamkhameleon.firebaseapp.com",
+//   databaseURL: "https://teamkhameleon.firebaseio.com",
+//   storageBucket: "teamkhameleon.appspot.com",
+//   messagingSenderId: "818068003629"
+// };
+// firebase.initializeApp(config);
 
-  // var testRef = firebase.database().ref('books').once('value').then(function(snapshot){
-  //   test =  snapshot.val();
-  //     console.log(test);
-  // });
+// var testRef = firebase.database().ref('books').once('value').then(function(snapshot){
+//   test =  snapshot.val();
+//     console.log(test);
+// });
+
+function changeInitialAddress() {
+    if (window.location.href === configs.url) {
+    window.location.href = configs + '#/' + content.general.lang + '/home';
+    console.log('change function');
+  }
+}
+
+
+
+function changeUrlLanguage(lang) {
+
+}
+
+function changeLanguage(lang) {
+  $.getJSON('../lang/' + lang + '.json', function (json) {
+    content = json;
+    router.resolve();
+  })
+}
 
--- script/router.js
+++ script/router.js
@@ -1,66 +1,102 @@
-var routerLanguageChange = new Navigo("https://localhost/", false);
+// Functions______________________________________________________________________________________
+function reloadHeaderAndFooterIfNotLoaded() {
 
-routerLanguageChange.on('bg/*',function () {
-        $.getJSON('../lang/bg.json', function (json) {
-        content = json;
-
-        router.resolve();
-
-        })
-         if (window.location.href === 'http://localhost/') {
-    window.location.href = 'http://localhost/#/' + configs.lang + '/home';
-    console.log('change function');
-}
-    }).on('en/*',function () {
-        $.getJSON('../lang/en.json', function (json) {
-        content = json;
-
-        router.resolve();
-
-        })
-         
-    });
-
-    var router = new Navigo("https://localhost/" + configs.lang, false);
-    
-
-    function reloadHeaderAndFooterIfNotLoaded() {
-
-        if (!$('#header').html()) {
-            // $.getJSON('../lang/bg.json',function (json) {
-            //     var content = json;
-            //     templateLoader.loadTemplate('header',document.getElementById('header'), content);
-            // });
-
+    if (!$('#header').html()) {
+        $.getJSON('../lang/bg.json',function (json) {
+            var content = json;
             templateLoader.loadTemplate('header',document.getElementById('header'), content);
-            
-        }
+        });
 
-        if (!$('#footer').html()) {
-            templateLoader.loadTemplate('footer',document.getElementById('footer'));
-        }
+        templateLoader.loadTemplate('header', document.getElementById('header'), content);
+
     }
 
-    router.on('/home',function () {
-         reloadHeaderAndFooterIfNotLoaded()
-        templateLoader.loadTemplate('home',document.getElementById('body-container'));
-        console.log('index');
-    })
+    if (!$('#footer').html()) {
+        templateLoader.loadTemplate('footer', document.getElementById('footer'));
+    }
+}
+
+
+// function mainRouter(lang) {
+//     var router = new Navigo("https://localhost/" + configs.lang, false);
+
+//     router.on('/home', function () {
+//         reloadHeaderAndFooterIfNotLoaded()
+//         templateLoader.loadTemplate('home', document.getElementById('body-container'));
+//         console.log('index');
+//     })
+//     .on('sectionTest', function () {
+//         templateLoader.loadTemplate('section', document.getElementById('body-container'));
+//         reloadHeaderAndFooterIfNotLoaded()
+//         console.log('section');
+//     }).on('product', function () {
+//         templateLoader.loadTemplate('product', document.getElementById('body-container'));
+//         reloadHeaderAndFooterIfNotLoaded()
+//         console.log('Product');
+//     }).on('checkout', function () {
+//         templateLoader.loadTemplate('checkout', document.getElementById('body-container'));
+//         reloadHeaderAndFooterIfNotLoaded()
+//         console.log('Checkout');
+//     });
+
+// }
+
+// // end Functions___________________________________________________________________________________________
+
+
+// var routerLanguageChange = new Navigo("https://localhost", false);
+
+// routerLanguageChange.on('bg/*', function () {
+//     $.getJSON('../lang/bg.json', function (json) {
+//         content = json;
+
+//         configs.lang = 'bg';
+
+
+//     })
+//     if (window.location.href === 'http://localhost/') {
+//         window.location.href = 'http://localhost/#/' + configs.lang + '/home';
+//         console.log('change function');
+//     }
+// }).on('en/*', function () {
+//     $.getJSON('../lang/en.json', function (json) {
+//         content = json;
+
+//         router.resolve();
+
+//     })
+
+// });
+
+// var router = new Navigo("https://localhost/" + configs.lang, false);
+
+
+
+
+// if (window.location.href === 'http://localhost/') {
+//     window.location.href = 'http://localhost/#/' + configs.lang + '/home';
+//     console.log('change function');
+// }
+
+
+var router = new Navigo('http://localhost:6060/', false);
+
+router.on('/home', function () {
+    reloadHeaderAndFooterIfNotLoaded()
+    templateLoader.loadTemplate('home', document.getElementById('body-container'));
+    console.log('index');
+})
     .on('sectionTest', function () {
-        templateLoader.loadTemplate('section',document.getElementById('body-container'));
-         reloadHeaderAndFooterIfNotLoaded()
+        templateLoader.loadTemplate('section', document.getElementById('body-container'));
+        reloadHeaderAndFooterIfNotLoaded()
         console.log('section');
     }).on('product', function () {
-        templateLoader.loadTemplate('product',document.getElementById('body-container'));
+        templateLoader.loadTemplate('product', document.getElementById('body-container'));
         reloadHeaderAndFooterIfNotLoaded()
         console.log('Product');
     }).on('checkout', function () {
-        templateLoader.loadTemplate('checkout',document.getElementById('body-container'));
+        templateLoader.loadTemplate('checkout', document.getElementById('body-container'));
         reloadHeaderAndFooterIfNotLoaded()
         console.log('Checkout');
     });
 
-    if (window.location.href === 'http://localhost/') {
-    window.location.href = 'http://localhost/#/' + configs.lang + '/home';
-    console.log('change function');
-}
